127eb6ba8248d2027eb3c220e7e441632c2d59cb
Andra Muntean <andramuntean@Andras-MacBook-Air.local>
2022-07-19 12:07:13 +0300

    Added a new and optional input with a checkbox

--- src/Pages/GoalsItems/Goals.js
+++ src/Pages/GoalsItems/Goals.js
@@ -5,6 +5,7 @@ const Goals = (props) => {
 
    const[enteredValue, setEnteredValue] = useState('');
    const[currentValue, setCurrentValue] = useState('');
+   const[checked, setChecked] = useState('');
 
    const goalInputHandler = (event) => {
     setEnteredValue(event.target.value);
@@ -17,10 +18,20 @@ const Goals = (props) => {
 
    }
 
+   const changeHandler = (event) => {
+    setChecked(!checked);
+   }
+
     return(
         <form className="goals" onSubmit={submitHandlerGoals}>
-            <input  className="input_goal" type='text' onChange={goalInputHandler} placeholder="your goal here"/>
-            <button className="button_goal" type='submit'> Add goal </button>
+            <input className="input_goal" type='text' onChange={goalInputHandler} placeholder="your goal here"/>
+            <div>
+                 { checked && < input className="optional_input" type="text" placeholder="optional text" />  }
+            </div>
+            <div>
+                <input className="checkbox_style" type="checkbox" checked={checked} onChange={changeHandler}/>
+                <button className="button_goal" type='submit'> Add goal </button>
+            </div>
             <div> {currentValue} </div>
         </form>
     );
