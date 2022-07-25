7d8dee3cb032d48aab360157f9ff0e8db15b120a
Andra Muntean <andramuntean@192-168-0-100.rdsnet.ro>
2022-07-25 14:24:16 +0300

    Added useReducer for email and password validation

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,21 @@
 import './LoginStyle.css';
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer} from 'react';
 import Alert from '@mui/material/Alert';
+
+const emailReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.includes('@')};
+    }
+    return { value: '', isValid: false};
+};
+
+const passwordReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.trim().length > 6};
+    }
+    return { value: '', isValid: false};
+};
 
 const Login = () => {
 
@@ -10,11 +24,24 @@ const Login = () => {
     const [enteredPassword, setEnteredPassword] = useState('');
     const [confirm,setConfirm] = useState(false);
 
+    const [emailState, dispatchEmail] = useReducer(emailReducer, {
+        value: '',          //initialState
+        isValid: null,     //initialFn
+    });
+
+    const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+        value: '',
+        isValid: null,
+    });
+
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
+
     useEffect ( () => {
         const identifier = setTimeout(() => {
             console.log('Checking form validity');
             setConfirm(
-                enteredEmail.includes('@') && enteredPassword.trim().length > 6
+               emailIsValid && passwordIsValid
             );
         },500);
 
@@ -22,20 +49,24 @@ const Login = () => {
             console.log('CLEANUP');
             clearTimeout(identifier);
         };
-    }, [enteredEmail, enteredPassword]);
+    }, [emailIsValid, passwordIsValid]);
 
     const changeHandlerEmail = (event) => {
+        dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
         setEnteredEmail(event.target.value);
     }
 
     const changeHandlerPassword = (event) => {
+        dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
+
         setEnteredPassword(event.target.value);
     }
 
     const submitHandlerLogin = (event) => {
 
         event.preventDefault();
-        if(enteredEmail.trim().length === 0 || !enteredEmail.includes('@') || enteredPassword.trim().length < 6)
+        if(emailState.value.trim().length === 0 || !emailState.value.includes('@') || passwordState.value.trim().length < 6)
         {
             setConfirm(!confirm);
             return;
