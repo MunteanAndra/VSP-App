abb496e4527aa241ec04bb65d8fcb00d74d46bec
Andra Muntean <andramuntean@Andras-MacBook-Air.local>
2022-08-11 14:29:40 +0300

    Added input fields for adding new meal item

--- src/Pages/Meals/Meals.js
+++ src/Pages/Meals/Meals.js
@@ -6,6 +6,14 @@ import CardContent from '@mui/material/CardContent';
 const Meals = () => {
     return(
         <div className="meals_text">
+            <Card className="card_wrapper">
+                <CardContent className="new_meal">
+                    <input className="meal_input" type="text" placeholder="Title" />
+                    <input className="meal_input" type="text" placeholder="Description" />
+                    <input className="meal_input" type="number" placeholder="Price" />
+                    <button className="meal_button">Add new meal</button>
+                </CardContent>
+            </Card>
             <Card variant="outlined">
                 <CardContent>
                     <AvailableMeals />
