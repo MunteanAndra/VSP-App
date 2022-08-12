2aa94e4c1f8cf204678d880d505553f7eaa0980e
Andra Muntean <andramuntean@Andras-MacBook-Air.local>
2022-08-12 11:51:42 +0300

    Display meal items from backend through JSON file

--- src/Pages/Meals/AvailableMeals.js
+++ src/Pages/Meals/AvailableMeals.js
@@ -1,45 +1,19 @@
 import MealItem from './MealItem';
 import './AvailableMealsStyle.css';
-import { useState, useEffect } from 'react';
 
 const AvailableMeals = (props) => {
 
-  const [meals,setMeals] = useState([]);
-
-  useEffect(()=>{
-    fetchMealsHandler();
-  },[]);
-
-  function fetchMealsHandler() {
-
-    fetch('http://localhost:3000/Meals')
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          const transformedMeals = data.mealsList.map((mealsData) => {
-            return {
-              id: mealsData.id,
-              key: mealsData.id,
-              name: mealsData.name,
-              description: mealsData.description,
-              price: mealsData.price,
-            };
-          });
-          setMeals(transformedMeals);
-        });
-  }
-
   return (
     <div className="meals">
-        {meals.map((meal) => (
-            <MealItem className="unordered_meal_list"
+        {props.meals.map((meal) => {
+            return <MealItem className="unordered_meal_list"
               id={meal.id}
               key={meal.id}
               name={meal.name}
               description={meal.description}
               price={meal.price}
-            />))
+            />
+          })
         }
     </div>
   );
--- src/Pages/Meals/Meals.js
+++ src/Pages/Meals/Meals.js
@@ -2,21 +2,61 @@ import './MealsStyle.css';
 import AvailableMeals from './AvailableMeals';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import {useEffect, useState} from "react";
 
-const Meals = () => {
+const Meals = (props) => {
+
+    const [meals,setMeals] = useState([]);
+    const [enteredMeal,setEnteredMeal] = useState({});
+
+    useEffect(()=>{
+        fetchMealsHandler();
+    },[]);
+
+    function fetchMealsHandler() {
+        fetch('http://localhost:3000/Meals')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data.mealsList);
+                setMeals(data.mealsList);
+            });
+    }
+
+    const mealHandler = (event) => {
+        setEnteredMeal({...enteredMeal, [event.target.name]: event.target.value});
+    }
+
+    const submitAddMeals = (event) => {
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(enteredMeal)
+        };
+
+        fetch('http://localhost:3000/Meals', requestOptions)
+            .then((data) => {
+                fetchMealsHandler();
+            });
+    }
+
     return(
         <div className="meals_text">
             <Card className="card_wrapper">
                 <CardContent className="new_meal">
-                    <input className="meal_input" type="text" placeholder="Title" />
-                    <input className="meal_input" type="text" placeholder="Description" />
-                    <input className="meal_input" type="number" placeholder="Price" />
-                    <button className="meal_button">Add new meal</button>
+                    <input className="meal_input" onChange={mealHandler} name="name" type="text" placeholder="Title" />
+                    <input className="meal_input" onChange={mealHandler} name="description" type="text" placeholder="Description" />
+                    <input className="meal_input" onChange={mealHandler} name="price" type="number" placeholder="Price" />
+                    <button className="meal_button" onClick={submitAddMeals}>Add new meal</button>
                 </CardContent>
             </Card>
             <Card variant="outlined">
                 <CardContent>
-                    <AvailableMeals />
+                    <AvailableMeals meals={meals}/>
                 </CardContent>
             </Card>
         </div>
